3b5c12ea0f2327ee0552e696e96e92d8ae0c5ba9
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-02-25 19:22:13 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
+improt {DataService} from './data.service';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  objectKeys = Object.keys;
+  cryptos: any;
   public appPages = [
     {
       title: 'Home',
@@ -25,11 +28,19 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private _data: DataService
   ) {
     this.initializeApp();
   }
-
+ngOnInit({
+  this._data.getPrices(
+    .subscribe(res => {
+      this.cryptos = res;
+      console.log(res);
+    })
+  )
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
