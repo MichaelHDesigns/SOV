65dc7bbfbe8a96552bd13ad13309dfdcde3dae71
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-02-26 00:52:01 -0500

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpClientModule } from '@angular/common/http';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
-import { HomePage } from './home.page';
+import { MyApp } from './app.component';
+import { HoldingsProvider } from '../providers/holdings/holdings';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
+  declarations: [
+    MyApp
   ],
-  declarations: [HomePage]
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp, {
+      preloadModules: true
+    }),
+    IonicStorageModule.forRoot(),
+    HttpClientModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HoldingsProvider
+  ]
 })
-export class HomePageModule {}
+export class AppModule {}
