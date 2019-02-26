38328fae99af28ee62d675879db7c266b6307154
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-02-25 19:20:47 -0500

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/https';
+import 'rxjs/add/operator/map';
 
-@Injectable({
+@Injectable()
   providedIn: 'root'
-})
 export class DataService {
 
-  constructor() { }
+  result:any;
+
+  constructor(private _http: Http) { }
+  
+  getprices(){
+    return this._http._https.get('http://sovcore.org/price.php')
+    .map(result => this.result = result.json());
+  }
 }
